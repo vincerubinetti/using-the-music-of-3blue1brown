58b485c8af50e4ddb4122029be4f4d1fbc480ca8
Vincent Rubinetti <vince.rubinetti@gmail.com>
2021-04-06 17:50:27 -0400

    deploy

---
+++ precache-manifest.892a0df4b207f8c5d9666658b23e72af.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db341468d7ee41dac59a17d7fb491d52",
+    "url": "/using-the-music-of-3blue1brown/index.html"
+  },
+  {
+    "revision": "12cc00c4fde9b5b6dc12",
+    "url": "/using-the-music-of-3blue1brown/static/css/main.bf179f43.chunk.css"
+  },
+  {
+    "revision": "de73db7cdd63b7174393",
+    "url": "/using-the-music-of-3blue1brown/static/js/2.1a15dfb2.chunk.js"
+  },
+  {
+    "revision": "a4d2a5c640243bdc29e54f31542c4677",
+    "url": "/using-the-music-of-3blue1brown/static/js/2.1a15dfb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12cc00c4fde9b5b6dc12",
+    "url": "/using-the-music-of-3blue1brown/static/js/main.4c2c578c.chunk.js"
+  },
+  {
+    "revision": "c05ddb6e3f49e2f77861",
+    "url": "/using-the-music-of-3blue1brown/static/js/runtime-main.1c5cd89e.js"
+  },
+  {
+    "revision": "07c4ed88a87613432f26eaa9927ed32f",
+    "url": "/using-the-music-of-3blue1brown/static/media/checkbox.07c4ed88.svg"
+  },
+  {
+    "revision": "2bdb69bb756c2bcfa039d19243659fa0",
+    "url": "/using-the-music-of-3blue1brown/static/media/loading.2bdb69bb.svg"
+  },
+  {
+    "revision": "d2e5f628e1f25651f17125e6ba824882",
+    "url": "/using-the-music-of-3blue1brown/static/media/logo.d2e5f628.png"
+  }
+]);
